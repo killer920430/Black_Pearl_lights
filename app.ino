001cb095dbb2acfad9aaf6e8a508a4ebcd81b62a
Jakub Strzelczyk <killer920430@gmail.com>
2020-06-15 01:15:41 +0200

    Initial Dummy Commit

---
+++ app.ino
@@ -0,0 +1,26 @@
+int ledPin = 13;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(1000);
+}
