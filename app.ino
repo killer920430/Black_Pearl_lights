4d24af0affe41329dce7dd347243263a4f538be2
Jakub Strzelczyk <killer920430@gmail.com>
2020-07-05 18:10:33 +0200

    Add multiple candles

--- app.ino
+++ app.ino
@@ -2,16 +2,26 @@
 #include "src/itf/Led.h"
 #include "src/candle/Candle.hpp"
 
-#define FLICKER_LED_PIN0 3
-#define FLICKER_LED_PIN1 5
-#define FLICKER_LED_PIN2 6
-#define FLICKER_LED_PIN3 9
-#define FLICKER_LED_PIN4 10
-#define FLICKER_LED_PIN5 11
+#define FLICKER_LED_PIN1 3
+#define FLICKER_LED_PIN2 5
+#define FLICKER_LED_PIN3 6
+#define FLICKER_LED_PIN4 9
+#define FLICKER_LED_PIN5 10
+#define FLICKER_LED_PIN6 11
 
-arduino::AnalogLed led1{FLICKER_LED_PIN0};
+arduino::AnalogLed led1{FLICKER_LED_PIN1};
+arduino::AnalogLed led2{FLICKER_LED_PIN2};
+arduino::AnalogLed led3{FLICKER_LED_PIN3};
+arduino::AnalogLed led4{FLICKER_LED_PIN4};
+arduino::AnalogLed led5{FLICKER_LED_PIN5};
+arduino::AnalogLed led6{FLICKER_LED_PIN6};
 
 candle::Candle candle1{led1};
+candle::Candle candle2{led2};
+candle::Candle candle3{led3};
+candle::Candle candle4{led4};
+candle::Candle candle5{led5};
+candle::Candle candle6{led6};
 
 void setup()
 {
@@ -21,5 +31,10 @@ void setup()
 void loop()
 {
     candle1.run();
+    candle2.run();
+    candle3.run();
+    candle4.run();
+    candle5.run();
+    candle6.run();
     delay(100); // the delay between changes
 }
