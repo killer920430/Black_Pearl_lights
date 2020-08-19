cb4abd947da9cfedba379d000812f251206b1a21
Jakub Strzelczyk <killer920430@gmail.com>
2020-08-19 22:01:40 +0200

    Inprovemetns in Candle

--- src/candle/Candle.cpp
+++ src/candle/Candle.cpp
@@ -3,22 +3,52 @@
 
 namespace candle
 {
+    uint8_t Candle::values[] = {};
+
     Candle::Candle(itf::Led &led) : led(led)
     {
-        value = random(low_value, high_value);
+        generateValues();
     }
 
     void Candle::run()
     {
-        led.write(value);
-        value += random(random_min_value, random_max_value);
+        led.write(values[index]);
+        index++;
+        if (index == valuesSize)
+            index = 0;
+    }
+
+    void Candle::off()
+    {
+        led.write(0);
+    }
+
+    void Candle::generateValues()
+    {
+        static bool areValuesGenerated = false;
+        if (!areValuesGenerated)
+        {
+            values[0] = random(low_value, high_value);
+            for (int i = 1; i < valuesSize; ++i)
+            {
+                values[i] = generateValue(values[i - 1]);
+            }
+            areValuesGenerated = true;
+        }
+        index = random(0, valuesSize - 1);
+    }
+
+    unsigned int Candle::generateValue(const unsigned int &lastValue)
+    {
+        unsigned int value = lastValue + random(random_min_value, random_max_value);
         if (value > high_value)
         {
-            value = high_value;
+            return high_value;
         }
         if (value < low_value)
         {
-            value = low_value;
+            return low_value;
         }
+        return value;
     }
 } // namespace candle
--- src/candle/Candle.hpp
+++ src/candle/Candle.hpp
@@ -10,15 +10,20 @@ namespace candle
     public:
         Candle(itf::Led &);
         void run();
+        void off();
 
     private:
-        static constexpr uint8_t low_value = 90;
+        void generateValues();
+        unsigned int generateValue(const unsigned int &lastValue);
+        static constexpr uint8_t valuesSize{30};
+        static constexpr uint8_t low_value = 70;
         static constexpr uint8_t high_value = 180;
-        static constexpr int8_t random_min_value = -30;
-        static constexpr uint8_t random_max_value = 30;
+        static constexpr int8_t random_min_value = -50;
+        static constexpr uint8_t random_max_value = 50;
 
+        static uint8_t values[valuesSize];
         itf::Led &led;
-        uint8_t value;
+        uint8_t index = 0;
     };
 } // namespace candle
 #endif /* SRC_CANDLE_CANDLE */
