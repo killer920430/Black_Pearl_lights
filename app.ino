f4bde4de6c25b9943f06c7d6b96132860c8fef46
Jakub Strzelczyk <killer920430@gmail.com>
2020-08-22 22:16:03 +0200

    Cleanup in Candle

--- app.ino
+++ app.ino
@@ -10,12 +10,11 @@
 #define AUDIO_RX 2
 #define AUDIO_TX 3
 
-// #define FLICKER_LED_PIN1 3
-#define FLICKER_LED_PIN2 5
-#define FLICKER_LED_PIN3 6
-#define FLICKER_LED_PIN4 9
-#define FLICKER_LED_PIN5 10
-#define FLICKER_LED_PIN6 11
+#define FLICKER_LED_PIN1 5
+#define FLICKER_LED_PIN2 6
+#define FLICKER_LED_PIN3 9
+#define FLICKER_LED_PIN4 10
+#define FLICKER_LED_PIN5 11
 
 #define SWITCH_1 12
 #define SWITCH_2 13
@@ -27,19 +26,17 @@
 
 arduino::ArduinoPinControl pinControl{};
 
-// led::AnalogLed led1{pinControl, FLICKER_LED_PIN1};
+led::AnalogLed led1{pinControl, FLICKER_LED_PIN1};
 led::AnalogLed led2{pinControl, FLICKER_LED_PIN2};
 led::AnalogLed led3{pinControl, FLICKER_LED_PIN3};
 led::AnalogLed led4{pinControl, FLICKER_LED_PIN4};
 led::AnalogLed led5{pinControl, FLICKER_LED_PIN5};
-led::AnalogLed led6{pinControl, FLICKER_LED_PIN6};
 
-// candle::Candle candle1{led1};
+candle::Candle candle1{led1};
 candle::Candle candle2{led2};
 candle::Candle candle3{led3};
 candle::Candle candle4{led4};
 candle::Candle candle5{led5};
-candle::Candle candle6{led6};
 
 cannon::Cannons cannons{
     pinControl,
@@ -113,20 +110,19 @@ void loop()
 {
     if (lightsOn)
     {
-        // candle1.run();
+        candle1.run();
         candle2.run();
         candle3.run();
         candle4.run();
         candle5.run();
-        candle6.run();
     }
     else
     {
+        candle1.off();
         candle2.off();
         candle3.off();
         candle4.off();
         candle5.off();
-        candle6.off();
     }
 
     cannons.run();
